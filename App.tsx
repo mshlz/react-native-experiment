7c26f165e7dcc26328da0f715f5c35909e5a45cc
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2022-01-21 21:16:58 -0300

    experiment: simple sum app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [valueA, setValueA] = useState(0);
+  const [valueB, setValueB] = useState(0);
+
+  const sumValues = () => {
+    const sum = valueA + valueB
+    alert("Sum: " + sum)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='black' />
+
+      <TextInput
+        underlineColorAndroid="transparent"
+        placeholder="Enter the first value"
+        placeholderTextColor="black"
+        autoCapitalize="none"
+        keyboardType="numeric"
+        onChangeText={value => setValueA(+value)}
+      />
+      <TextInput
+        underlineColorAndroid="transparent"
+        placeholder="Enter the second value"
+        placeholderTextColor="black"
+        autoCapitalize="none"
+        keyboardType="numeric"
+        onChangeText={value => setValueB(+value)}
+      />
+
+      <TouchableOpacity
+        onPress={() => sumValues()}
+      >
+        <Text>Sum</Text>
+      </TouchableOpacity>
     </View>
   );
 }
